cc502bb0822820fb4b924a4fc1dbc4362d044b77
Stephen Bowen <bowensteven17@gmail.com>
2024-03-18 10:35:31 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names=["Guadalupe", "Ollie", "Aki"];
+function writeCards(names,string){
+    const messages = [];
+    for(let i = 0;i < names.length; i++){
+        const birthdayMesssage =( (`Thank you, ${names[i]}, for the wonderful ${string} gift!`));
+        messages.push(birthdayMesssage);
+    }   
+    
+    return messages;
+}
+console.log(writeCards(names, 'surprise'));
+
+function countDown(num){
+    while (num>=0){
+        console.log(num--);
+    }
+}
