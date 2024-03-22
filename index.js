15835b159ed3ba79c3deea6621b95f79d98e508c
Stephen Bowen <bowensteven17@gmail.com>
2024-03-22 08:03:12 +0300

    Complete looping code lab

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const names=["Guadalupe", "Ollie", "Aki"];
-function writeCards(names,string){
+
+function writeCards(newNames,string){
     const messages = [];
-    for(let i = 0;i < names.length; i++){
-        const birthdayMesssage =( (`Thank you, ${names[i]}, for the wonderful ${string} gift!`));
+    for(let i = 0;i < newNames.length; i++){
+        const birthdayMesssage =( (`Thank you, ${newNames[i]}, for the wonderful ${string} gift!`));
         messages.push(birthdayMesssage);
     }   
     
     return messages;
 }
-console.log(writeCards(names, 'surprise'));
 
 function countDown(num){
     while (num>=0){
